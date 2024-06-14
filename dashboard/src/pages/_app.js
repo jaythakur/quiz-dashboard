271f7365e4f4cfbbbd9f0519d38d867cda2f2624
jaypalgoal <jaypal@goalign.ai>
2024-06-13 20:47:34 -0400

    fix(ui): Added MUI Library

--- dashboard/src/pages/_app.js
+++ dashboard/src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.scss';
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider {...props}>
+      <Component {...pageProps} />
+    </AppCacheProvider>
+  );
 }
